b321f6e3509cfef7592971e65f655149b68efa18
iesianul <marius-mihai.cazacu@academic.tuiasi.ro>
2026-07-09 16:51:04 +0300

    Add files via upload

---
+++ simulation/sim1/js/lib/_vti_cnf/require.js
@@ -0,0 +1,4 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|04 Apr 2016 13:22:00 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|simulation/sim1/index.html
---
+++ simulation/sim4/authUser.js
@@ -0,0 +1,11 @@
+function authUser(form) { 
+if (form.Username.value=="constructii") { 
+if (form.Password.value=="constructii") { 
+    location="index1.htm" 
+} else { 
+    location="index2.html"
+    } 
+} else { 
+    location="index2.html"
+    } 
+} 
